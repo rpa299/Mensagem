b209c7f2688bbfb772e884f4cffa9bc9835fb105
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-03-11 09:32:09 +0000

    Criação do Projeto e criação de uma activity para mostrar Info

---
+++ app/src/main/java/pt/ipg/mensagem/MostraInfoActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.mensagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostraInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostra_info);
+    }
+}
